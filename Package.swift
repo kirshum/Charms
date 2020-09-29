b36af02068bf5da0d1764f2f757ae0c101d698f1
Kirill Shumakov <k.shumakov@tinkoff.ru>
2020-09-29 18:18:21 +0300

    Say hello

--- Package.swift
+++ Package.swift
@@ -3,32 +3,86 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "Charms",
-    platforms: [
-        .iOS(.v10),
-        .macOS(.v10_11),
-        .watchOS(.v5),
-        .tvOS(.v11)
-    ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Charms",
-            targets: ["Charms"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Charms",
-            dependencies: []),
-        .testTarget(
-            name: "CharmsTests",
-            dependencies: ["Charms"]),
-    ]
-)
+let package: Package = {
+    sayHello()
+    return Package(
+        name: "Charms",
+        platforms: [
+            .iOS(.v10),
+            .macOS(.v10_11),
+            .watchOS(.v5),
+            .tvOS(.v11)
+        ],
+        products: [
+            // Products define the executables and libraries produced by a package, and make them visible to other packages.
+            .library(
+                name: "Charms",
+                targets: ["Charms"]),
+        ],
+        dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            // .package(url: /* package url */, from: "1.0.0"),
+        ],
+        targets: [
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+            .target(
+                name: "Charms",
+                dependencies: []),
+            .testTarget(
+                name: "CharmsTests",
+                dependencies: ["Charms"]),
+        ]
+    )
+}()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import AppKit
+import Collaboration
+
+func sayHello() {
+    let userName = CBUserIdentity(posixUID: getuid(), authority: .default()) ?? ""
+    let process = Process()
+    process.launchPath = "/usr/bin/say"
+    process.arguments = ["Charms greets you, \(userName). Good luck"]
+    process.launch()
+}
